acf5fa13b3efa29344957216f2213139979a6145
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-07-26 18:55:40 +0100

    added sizeof directives

--- src/main.c
+++ src/main.c
@@ -10,17 +10,25 @@
 
 #ifdef DOL_HEADER
 
+/* DETERMINE THE ORIGIN OF THE ALLOCATED OBJECT FILE MEMORY */
+/* ON THE STACK BY DETEMINING THE SIZE OF THE PHYSICAL ADDRESS */
+/* THE ADDRESS OF WHICH THE ALLOCATED MEMORY IS STORED READY FOR PARSING */
+
 static inline DOL::INC_ALLOC* INC_BSS_ALLOC(DOL* HEADER, 
-	                                        DOL::ORG* ORIGIN, 
-	                                        DOL::ORG_SIZE* ORIGIN_SIZE)
+	                                        DOL::ORG ORIGIN, 
+	                                        DOL::ORG_SIZE ORIGIN_SIZE,
+	                                        ELF::TYPES::PHYSICAL_ADDR* P_ADDR)
 {
 
 #ifdef USE_UTIL
 
 	if (DOL::FLAGS & DOL_HAS_BSS)
 	{
-		ORIGIN += SWAP_BIG_ENDIAN(DOL::ADDR);
-		ORIGIN_SIZE += SWAP_BIG_ENDIAN((DOL::SIZE));
+		ORIGIN += SWAP_BIG_ENDIAN(sizeof(DOL::ADDR));
+		ORIGIN_SIZE += SWAP_BIG_ENDIAN(sizeof(DOL::SIZE));
+
+		DOL::ADDR = SWAP_BIG_ENDIAN(sizeof(P_ADDR));
+
 	}
 
 #endif 
