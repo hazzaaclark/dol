828b00e7b290f8309d23a1ae7d2d04fd4a254acc
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-07-26 16:52:25 +0100

    init the stack when the object file is recognised

--- src/main.c
+++ src/main.c
@@ -1,7 +1,30 @@
+/* Copyright (C) 2023 Harry Clark */
+
+/* Dynamic DLL to DOL toolkit for Riivolution */
+
+/* THIS FILE PERTAINS TO THE MAIN FUNCTIONALITY OF THE PROGRAM */
+
+/* NESTED INCLUDES */
+
 #include "util.h"
 
-void TEST()
+#ifdef DOL_HEADER
+
+static inline DOL::INC_ALLOC* INC_BSS_ALLOC(DOL* HEADER, 
+	                                        DOL::ORG* ORIGIN, 
+	                                        DOL::ORG_SIZE* ORIGIN_SIZE)
 {
-	U32 VALUE = 0x122344445565;
-	U32 SWAPPED = SWAP_BIG_ENDIAN(VALUE);
+
+#ifdef USE_UTIL
+
+	if (DOL::FLAGS & DOL_HAS_BSS)
+	{
+		ORIGIN += SWAP_BIG_ENDIAN(DOL::ADDR);
+		ORIGIN_SIZE += SWAP_BIG_ENDIAN((DOL::SIZE));
+	}
+
+#endif 
+
 }
+
+#endif
