9095fca108d1ac7a04371d4b1985127083a32505
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-07-26 19:06:04 +0100

    tidy up

--- src/main.c
+++ src/main.c
@@ -14,21 +14,16 @@
 /* ON THE STACK BY DETEMINING THE SIZE OF THE PHYSICAL ADDRESS */
 /* THE ADDRESS OF WHICH THE ALLOCATED MEMORY IS STORED READY FOR PARSING */
 
-static inline DOL::INC_ALLOC* INC_BSS_ALLOC(DOL* HEADER, 
-	                                        DOL::ORG ORIGIN, 
-	                                        DOL::ORG_SIZE ORIGIN_SIZE,
-	                                        ELF::TYPES::PHYSICAL_ADDR* P_ADDR)
+static inline DOL::INC_ALLOC* INC_BSS_ALLOC(DOL* HEADER, ELF::TYPES::PHYSICAL_ADDR* P_ADDR)
 {
 
 #ifdef USE_UTIL
 
 	if (DOL::FLAGS & DOL_HAS_BSS)
 	{
-		ORIGIN += SWAP_BIG_ENDIAN(sizeof(DOL::ADDR));
-		ORIGIN_SIZE += SWAP_BIG_ENDIAN(sizeof(DOL::SIZE));
-
+		DOL::ORG += SWAP_BIG_ENDIAN(sizeof(DOL::ADDR));
+		DOL::ORG_SIZE += SWAP_BIG_ENDIAN(sizeof(DOL::SIZE));
 		DOL::ADDR = SWAP_BIG_ENDIAN(sizeof(P_ADDR));
-
 	}
 
 #endif 
